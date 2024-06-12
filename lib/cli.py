6febda974da9bd208fbc4f612dd52e52e6b9edfd
iankuria668 <iankuria668@gmail.com>
2024-06-12 12:57:51 +0300

    Helperss created

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,7 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    
 )
 
 
@@ -13,9 +13,34 @@ def main():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            helper_1()
+            list_tasks()
+        elif choice == "2":
+            find_task_by_name()
+        elif choice == "3":
+            find_task_by_id()
+        elif choice == "4":
+            create_task()
+        elif choice == "5":
+            update_task()
+        elif choice == "6":
+            delete_task()
+        elif choice == "7":
+            list_categories()
+        elif choice == "8":
+            find_category_by_name()
+        elif choice == "9":
+            find_category_by_id()
+        elif choice == "10":
+            create_category()
+        elif choice == "11":
+            update_category()
+        elif choice == "12":
+            delete_category()
         else:
             print("Invalid choice")
+
+
+
 
 
 def menu():
--- lib/helpers.py
+++ lib/helpers.py
@@ -4,10 +4,44 @@ def exit_program():
     print("Goodbye!")
     exit()
 
+def list_tasks():
+    pass
+
+def find_task_by_name():
+    pass
+
+def find_task_by_id():
+    pass
+
+def create_task():
+    pass
+
+def update_task():
+    pass
+
+def delete_task():
+    pass
+
 def list_categories():
     pass
 
-def list_tasks():
+def find_category_by_name():
     pass
+
+def find_category_by_id():
+    pass
+
+def create_category():
+    pass
+
+def update_category():
+    pass
+
+def delete_category():
+    pass
+
+
+
+
 
 
